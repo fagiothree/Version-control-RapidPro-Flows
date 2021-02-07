4551b3a586b0ac407abd381bee4a9cd41a210388
chiarafacciola <chiara.facciola@gmail.com>
2021-02-07 15:11:38 +0100

    fixed typo in user info gender and survey count ipv

--- code_scripts/split_flows.js
+++ code_scripts/split_flows.js
@@ -4,7 +4,8 @@ var path = require("path");
 
 
 //var input_path_original_flows = path.join(__dirname, "./philippines/philippines-review_activities.json");
-var input_path_original_flows = path.join(__dirname, "../downloaded_flows/plh-south-africa-flavour.json");
+//var input_path_original_flows = path.join(__dirname, "../downloaded_flows/plh-south-africa-flavour.json");
+var input_path_original_flows = path.join(__dirname, "../downloaded_flows/plh-international-flavour.json");
 
 var json_string_or_fl = fs.readFileSync(input_path_original_flows).toString();
 var original_flows_full_json = JSON.parse(json_string_or_fl);
@@ -13,8 +14,8 @@ for (var fl = 0; fl<original_flows_full_json.flows.length; fl++){
     var flow_name = original_flows_full_json.flows[fl].name;
     var flow_obj =  JSON.stringify(original_flows_full_json.flows[fl], null, 2);
     
-    var output_path = path.join(__dirname, '../split_flows/south-africa/' + flow_name +'.json');
-    //var output_path = path.join(__dirname, './' + flow_name +'.json');
+    //var output_path = path.join(__dirname, '../split_flows/south-africa/' + flow_name +'.json');
+    var output_path = path.join(__dirname, '../split_flows/international/' + flow_name +'.json');
     fs.writeFile(output_path, flow_obj, function (err, result) {
         if (err) console.log('error', err);
     });
@@ -26,10 +27,9 @@ original_flows_full_json.flows = [];
 
 var full_flow_obj =  JSON.stringify(original_flows_full_json, null, 2);
 
-/*
+
 //var output_path = path.join(__dirname, './philippines/split_flows_localised/plh_master_no_flows.json');
 var output_path = path.join(__dirname, '../split_flows/plh_international_flavour_no_flows.json');
     fs.writeFile(output_path, full_flow_obj, function (err, result) {
         if (err) console.log('error', err);
     });
-*/
